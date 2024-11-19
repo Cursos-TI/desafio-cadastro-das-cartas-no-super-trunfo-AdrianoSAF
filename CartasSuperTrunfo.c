59effe32ca87d4e05335c31640f2560459eddbd5
Adriano Dos Santos Araújo Filho <53800947+AdrianoSAF@users.noreply.github.com>
2024-11-19 23:14:30 +0000

    Desafio cadastro das cartas. nível: Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,13 +9,47 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
+    //Definição das variáveis utilizadas no programa. 
+    char city_cod[4], city_name[30]; //Valor String (uma cadei de caracteres)
+    float city_population, city_area, city_pib; //Pronto Flutuante
+    int city_turistic_point; //inteiro
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
+    //Solicitando o input do usuário
+    printf("Digite o nome da cidade:\n");
+    scanf("%30[^\n]s",city_name);// Formatando a leitura dos dados (limita o armazenamento em 30)
+                                
+    printf("Digite o código da cidade (ex: A01):\n");
+    scanf(" %3[^\n]s", city_cod);
+
+    printf("Digite a população da cidade:\n");
+    scanf(" %f",&city_population);
+
+    printf("Digite a área da cidade:\n");
+    scanf(" %f",&city_area);
+
+    printf("Digite o PIB da cidade:\n");
+    scanf(" %f", &city_pib);
+
+    printf("Digite o número de pontos turísticos da cidade:\n");
+    scanf("%d", &city_turistic_point);
+
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("\nInformações da carta %s:\n\n\
+     Nome: %s\n\
+     Código: %s\n\
+     População: %f\n\
+     Área: %f\n\
+     PIB: %f\n\
+     Pontos turisticos: %d", \
+     city_name, city_name, city_cod, city_population, city_area, city_pib, city_turistic_point\
+     );//Variáveis
 
     return 0;
 }
